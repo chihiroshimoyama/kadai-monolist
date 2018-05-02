86fc960646fbd94e6cd02e76d8a6fb31c1dd9fef
chihiro.shimoyoma <chihiro.shimoyoma>
2018-05-02 10:46:37 +0000

    user login

--- config/app.php
+++ config/app.php
@@ -234,6 +234,8 @@ return [
         //追加
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        //追加
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
