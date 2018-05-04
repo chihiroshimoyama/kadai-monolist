262924f27eab8b29fb41a6fd9bad9630a16dc72b
chihiro.shimoyoma <chihiro.shimoyoma>
2018-05-04 08:21:34 +0000

    ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -13,9 +13,18 @@ class RankingController extends Controller
 {
     public function want()
     {
-        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->whereRaw('type = "want"')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as want_count'))->whereRaw('type = "want"')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('want_count', 'DESC')->take(10)->get();
 
         return view('ranking.want', [
+            'items' => $items,
+        ]);
+    }
+    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as have_count'))->whereRaw('type = "have"')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('have_count', 'DESC')->take(10)->get();
+
+        return view('ranking.have', [
             'items' => $items,
         ]);
     }
